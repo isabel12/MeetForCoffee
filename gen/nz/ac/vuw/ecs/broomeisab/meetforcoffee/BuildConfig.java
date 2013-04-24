1a8253eededd0bf6d5a668e4fc4f2103587f11bc
Isabel Broome-Nicholson <isabel.broomenicholson@gmail.com>
2013-04-24 15:07:19 +1200

    added existing files

---
+++ gen/nz/ac/vuw/ecs/broomeisab/meetforcoffee/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nz.ac.vuw.ecs.broomeisab.meetforcoffee;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
